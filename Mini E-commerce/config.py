5724a2ac3d69825dadf3c3104b4e14849199f174
m0hamed00ayman <m.ayman91989909@gmail.com>
2025-12-11 23:43:26 +0200

    Update config.py

--- Mini E-commerce/config.py
+++ Mini E-commerce/config.py
@@ -1,10 +1,10 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="2005",
-        database="mini_ecommerce",
-        charset='utf8mb4'
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
